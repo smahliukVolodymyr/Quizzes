49b98bbfb4295cc38ef80ed7cb03df693627e91b
Volodymyr Smahliuk <smaglukvolodia54@gmail.com>
2024-09-10 14:18:59 +0300

    project init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Quizzes' title`, () => {
+  it(`should have the 'quizzes' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Quizzes');
+    expect(app.title).toEqual('quizzes');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Quizzes');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, quizzes');
   });
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PlayComponent } from './pages/play/play.component';
+import { FinishComponent } from './pages/finish/finish.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'play', component: PlayComponent },
+  { path: 'finish', component: FinishComponent },
+  { path: '**', redirectTo: '' },
+];
